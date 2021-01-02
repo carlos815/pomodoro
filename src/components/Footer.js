75b592556b4ddf286be256e20b726cca313829e7
Carlos Hernandez <carlos@cleancommit.io>
2021-01-02 15:36:41 -0400

    publish

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='footer'>
-      <a href='http://'>Source Code</a>
+      <a href='https://github.com/carlos815/pomodoro'>Source Code</a>
     </div>
   )
 }
